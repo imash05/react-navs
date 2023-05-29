717f6d99c32bb17ea5e3964689348b57a1e1e6e7
IFer <76787367+imash05@users.noreply.github.com>
2023-05-29 19:55:43 +0200

    first commit

---
+++ react-navs/example/src/App.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+import { ExampleComponent } from 'react-navs'
+import 'react-navs/dist/index.css'
+
+const App = () => {
+  return <ExampleComponent text="Create React Library Example 😄" />
+}
+
+export default App
